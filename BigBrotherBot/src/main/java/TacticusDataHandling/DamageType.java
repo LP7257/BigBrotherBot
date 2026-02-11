200183eaf9ee15dd0d9b1d232d13e016c997029e
Palola Lassi Mikael Oskari <lassi.m.palola@student.jyu.fi>
2026-02-11 22:40:52 +0200

    more tidying up. Removed clones

--- BigBrotherBot/src/main/java/TacticusDataHandling/DamageType.java
+++
@@ -1,9 +0,0 @@
-package TacticusDataHandling;
-
-/**
- * Contains the two different damage types, since we only want Battles to be in the final output, we need to sort out bombs.
- */
-public enum DamageType {
-        Battle,
-        Bomb
-}
